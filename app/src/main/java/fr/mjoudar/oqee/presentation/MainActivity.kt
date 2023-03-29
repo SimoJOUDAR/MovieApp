b953e1b6df0905d71ef05ac82d15379d620e7ff4
SimoJOUDAR <ssjoudar@gmail.com>
2023-03-29 16:18:50 +0200

    Initial commit
    Add empty HomepageFragment and DetailFragment.
    
    Add Navigation and nav_graph.
    
    Add basic initial dependencies.

---
+++ app/src/main/java/fr/mjoudar/oqee/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.mjoudar.oqee.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.mjoudar.oqee.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/fr/mjoudar/oqee/presentation/homepage/HomepageFragment.kt
@@ -0,0 +1,38 @@
+package fr.mjoudar.oqee.presentation.homepage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import fr.mjoudar.oqee.R
+import fr.mjoudar.oqee.databinding.FragmentHomepageBinding
+
+class HomepageFragment : Fragment() {
+
+    private var _binding: FragmentHomepageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentHomepageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+
+
+    /***********************************************************************************************
+     ** Listeners
+     ***********************************************************************************************/
+
+    // Listen to profile button clicks to navigate to MyAccountFragment
+    private fun setOnClickListener() {
+        binding.button.setOnClickListener {
+            findNavController().navigate(HomepageFragmentDirections.actionHomepageFragmentToDetailFragment())
+        }
+    }
+}
